eb0c2851b5217b69c43a9e16f929d9e54f4c7cfa
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-11-27 02:53:14 +0700

    Update tts.py

--- MusicAndVideo/tts.py
+++ MusicAndVideo/tts.py
@@ -23,10 +23,10 @@ def convert(text):
 @Client.on_message(filters.command(["tts"], prefixes=f"{HNDLR}"))
 async def text_to_speech(_, message: Message):
     if not message.reply_to_message:
-        return await message.reply_text("💡 membalas beberapa teks !")
+        return await message.reply_text("🤦 **Mohon Balas Ke Pesan !**")
     if not message.reply_to_message.text:
-        return await message.reply_text("💡 membalas beberapa teks !")
-    m = await message.reply_text("🔁 Sedang memproses...")
+        return await message.reply_text("🤦 **Mohon Balas Ke Pesan !**")
+    m = await message.reply_text("🔁 Sedang Memproses...")
     text = message.reply_to_message.text
     try:
         loop = get_running_loop()
